e728a7f804346e0c9215641f486063f7069adf32
Abelas-Solihin <abelassolihin@gmail.com>
2025-04-15 21:27:39 +0700

    barulg

--- coba.java
+++ coba.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
     System.out.println("12312452");
     System.out.println("wkwkwkwkwkw");
 
-    System.out.println("hsgfisgh");
+    System.out.println("hsgfisghhbbc");
 }
 }
 
