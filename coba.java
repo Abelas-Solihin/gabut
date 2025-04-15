d1bd89a2f071f09f67c224764aa056e401a15eb0
gaduhprakoso <gaduhprakoso@gmail.com>
2025-04-15 21:24:17 +0700

    gaudh 2

--- coba.java
+++ coba.java
@@ -2,6 +2,9 @@ public class coba{
 public static void main(String[] args) {
     System.out.println("sya ganteng");
 
+    System.out.println("12312452");
     System.out.println("wkwkwkwkwkw");
 }
 }
+
+
